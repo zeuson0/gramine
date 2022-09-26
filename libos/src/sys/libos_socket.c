dc149a0d6a0eb2631cfb500d7535665aea5e1ba6
Guancheng Fu <110874468+gc-fu@users.noreply.github.com>
2022-09-26 04:05:18 -0400

    Prevent error when passing MSG_MORE to do_sendmsg (#4)

--- libos/src/sys/libos_socket.c
+++ libos/src/sys/libos_socket.c
@@ -613,7 +613,7 @@ ssize_t do_sendmsg(struct libos_handle* handle, struct iovec* iov, size_t iov_le
     if (handle->type != TYPE_SOCK) {
         return -ENOTSOCK;
     }
-    if (!WITHIN_MASK(flags, MSG_NOSIGNAL | MSG_DONTWAIT)) {
+    if (!WITHIN_MASK(flags, MSG_NOSIGNAL | MSG_DONTWAIT | MSG_MORE)) {
         return -EOPNOTSUPP;
     }
 
